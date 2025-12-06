29580601a17f8c825db824f59793bbcb2fca6c7f
user <user@localhost>
2025-12-05 17:11:31 -0700

    R: finer types

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -2,13 +2,22 @@
 type Die = 1 | 2 | 3 | 4 | 5 | 6
 type IndexDie = 0 | 1 | 2 | 3 | 4 | 5
 type FiveDices = [Die, Die, Die, Die, Die]
+type Count = 0 | 1 | 2 | 3 | 4 | 5
+type NumberOfDiesWith1 = Count
+type NumberOfDiesWith2 = Count
+type NumberOfDiesWith3 = Count
+type NumberOfDiesWith4 = Count
+type NumberOfDiesWith5 = Count
+type NumberOfDiesWith6 = Count
+type Counts = [
+  NumberOfDiesWith1,
+  NumberOfDiesWith2,
+  NumberOfDiesWith3,
+  NumberOfDiesWith4,
+  NumberOfDiesWith5,
+  NumberOfDiesWith6,
+]
 export default class Yatzy {
-  private fiveDices: FiveDices;
-
-  constructor(...fiveDices: FiveDices) {
-    this.fiveDices = fiveDices;
-  }
-
   static chance(...fiveDices: FiveDices): number {
     return fiveDices.reduce((accumulator, current) => accumulator + current, 0);
   }
@@ -44,26 +53,26 @@ export default class Yatzy {
   }
   // Two pairs: If there are two pairs of dice with the same number, the player scores the sum of these dice
   static two_pair(...fiveDices: FiveDices): number {
-    var counts = Array(6).fill(0)
+    var counts = Array(6).fill(0) as Counts
     fiveDices.forEach((value:Die)=>{counts[dieValueToIndex(value)]++})
     return counts.map((value, index) => value >= 2 ? indexToDieValue(index) as number : 0).reduce((acc, value)=>acc+value, 0) * 2
   }
   // Four of a kind: If there are four dice with the same number, the player scores the sum of these dice. 
   static four_of_a_kind(...fiveDices: FiveDices): number {
-    var counts = Array(6).fill(0)
+    var counts = Array(6).fill(0) as Counts
     fiveDices.forEach((value:Die)=>{counts[dieValueToIndex(value)]++})
     return counts.map((value, index) => value >= 4 ? indexToDieValue(index) as number : 0).reduce((acc, value)=>acc+value, 0) * 4
   }
   //Three of a kind: If there are three dice with the same number, the player scores the sum of these dice.
   static three_of_a_kind(...fiveDices: FiveDices): number {
-    var counts = Array(6).fill(0)
+    var counts = Array(6).fill(0) as Counts
     fiveDices.forEach((value:Die)=>{counts[dieValueToIndex(value)]++})
     return counts.map((value, index) => value >= 3 ? indexToDieValue(index) as number : 0).reduce((acc, value)=>acc+value, 0) * 3
   }
   // Small straight: When placed on “small straight”, if the dice read 1,2,3,4,5,
   // the player scores 15 (the sum of all the dice).
   static smallStraight(...fiveDices: FiveDices): number {
-    var counts = Array(6).fill(0)
+    var counts = Array(6).fill(0) as Counts
     fiveDices.forEach((value:Die)=>{counts[dieValueToIndex(value)]++})
     return counts
       .slice(0,5)
@@ -74,7 +83,7 @@ export default class Yatzy {
   // Large straight: When placed on “large straight”, if the dice read 2,3,4,5,6,
   // the player scores 20 (the sum of all the dice).
   static largeStraight(...fiveDices: FiveDices): number {
-    var counts = Array(6).fill(0)
+    var counts = Array(6).fill(0) as Counts
     fiveDices.forEach((value:Die)=>{counts[dieValueToIndex(value)]++})
     return counts
       .slice(1,6)
@@ -84,14 +93,14 @@ export default class Yatzy {
   }
   //Full house: If the dice are two of a kind and three of a kind, the player scores the sum of all the dice. 
   static fullHouse(...fiveDices: FiveDices): number {
-    var counts = Array(6).fill(0)
+    var counts = Array(6).fill(0) as Counts
     fiveDices.forEach((value:Die)=>{counts[dieValueToIndex(value)]++})
     return counts
       .filter((value, index, array)=>array.some((value)=> value === 2))
       .filter((value, index, array)=>array.some((value)=> value === 3))
-      .reduce((acc, value, index)=>{
+      .reduce((acc: number, value, index)=>{
         return (indexToDieValue(index))*value + acc
-      },0)
+      }, 0 as number)
   }
 }
 
